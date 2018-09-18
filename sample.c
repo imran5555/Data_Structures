737754fec4182a8c9cdf89576a55ede1939c77a1
Imran Alam <imranalam5555@gmail.com>
2018-09-18 23:42:55 +0530

    sample code added

---
+++ sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+typedef struct node	//created a node structure
+{
+	int data;
+	struct node* next;
+}node;
+
+int main()
+{
+	return 0;
+}
